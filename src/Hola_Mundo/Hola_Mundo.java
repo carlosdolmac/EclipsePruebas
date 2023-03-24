4417fc1f8d11eb7403f6c6a98799954294868f29
carlosdolmac <carlos.dolmac@educa.jcyl.es>
2023-03-24 18:41:39 +0100

    Haciendo mi primer commit

---
+++ src/Hola_Mundo/Hola_Mundo.java
@@ -0,0 +1,8 @@
+package Hola_Mundo;
+
+public class Hola_Mundo {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo2");
+		System.out.println("Hola Mundo");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author tarde
+ *
+ */
+module Hola_Mundo {
+}
