bc2f7cfccdbcdd54f19d70e84d55945f2359993f
carlosdolmac <carlos.dolmac@educa.jcyl.es>
2023-03-27 18:51:53 +0200

    Commit 2

--- src/Hola_Mundo/Hola_Mundo.java
+++ src/Hola_Mundo/Hola_Mundo.java
@@ -4,5 +4,7 @@ public class Hola_Mundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo2");
 		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo3");
 	}
 }
